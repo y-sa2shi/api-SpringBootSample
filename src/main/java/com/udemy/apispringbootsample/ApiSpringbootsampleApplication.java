0bee3087f2b01dd14d2b95c78b81d9b894b3a619
ziruvis <ziruvis3104@gmail.com>
2024-04-18 05:07:15 +0900

    first commit

---
+++ src/main/java/com/udemy/apispringbootsample/ApiSpringbootsampleApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.apispringbootsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringbootsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringbootsampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/apispringbootsample/ApiSpringbootsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.apispringbootsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringbootsampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
